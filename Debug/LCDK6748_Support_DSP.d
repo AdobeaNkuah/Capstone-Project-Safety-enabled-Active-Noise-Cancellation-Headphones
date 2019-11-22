cb4d0e6c27a8bb8187e432d61f6c43f11c3e50e0
Eddie Gasca <17egc1@queensu.ca>
2019-11-22 17:22:44 -0500

    Revising the root directory

---
+++ Debug/LCDK6748_Support_DSP.d
@@ -0,0 +1,21 @@
+# FIXED
+
+LCDK6748_Support_DSP.obj: ../LCDK6748_Support_DSP.c
+LCDK6748_Support_DSP.obj: ../DSP_Config.h
+LCDK6748_Support_DSP.obj: ../OMAPL138_Support_DSP.h
+LCDK6748_Support_DSP.obj: ../tistdtypes.h
+LCDK6748_Support_DSP.obj: ../OMAPL138_defines.h
+LCDK6748_Support_DSP.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h
+LCDK6748_Support_DSP.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h
+LCDK6748_Support_DSP.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h
+LCDK6748_Support_DSP.obj: ../AIC3106.h
+
+../LCDK6748_Support_DSP.c: 
+../DSP_Config.h: 
+../OMAPL138_Support_DSP.h: 
+../tistdtypes.h: 
+../OMAPL138_defines.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h: 
+../AIC3106.h: 
---
+++ Debug/StartUp.d
@@ -0,0 +1,19 @@
+# FIXED
+
+StartUp.obj: ../StartUp.c
+StartUp.obj: ../DSP_Config.h
+StartUp.obj: ../OMAPL138_Support_DSP.h
+StartUp.obj: ../tistdtypes.h
+StartUp.obj: ../OMAPL138_defines.h
+StartUp.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h
+StartUp.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h
+StartUp.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h
+
+../StartUp.c: 
+../DSP_Config.h: 
+../OMAPL138_Support_DSP.h: 
+../tistdtypes.h: 
+../OMAPL138_defines.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h: 

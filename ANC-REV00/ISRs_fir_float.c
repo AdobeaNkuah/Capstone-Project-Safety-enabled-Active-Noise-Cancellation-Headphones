dd81d3baa92044719ce78d0be470a68f80dfea55
Eddie Gasca <17egc1@queensu.ca>
2019-11-23 16:15:51 -0500

    Implemented the switch between ANC mode and Passthrough mode by pressing 'a' or 'p' on the keyboard. The code changes are found in ISRs_fir_float.c.

--- ANC-REV00/ISRs_fir_float.c
+++ ANC-REV00/ISRs_fir_float.c
@@ -36,7 +36,8 @@ volatile union {
 /* add any global variables here */
 float x_R_buffer[N];       //buffer for reference signal delay samples
 float x_E_buffer[N];       //buffer for error signal delay samples
-int data_flag = 0;         // UART data received flag
+int data_flag = 0;         //UART data received flag
+char data = 0;              //keyboard data (a = ANC mode) or (p = passthrough mode)
 
 float w[N] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};             //buffer weights of adapt filter
 
@@ -56,8 +57,12 @@ interrupt void Codec_ISR()
 	/* add any local variables here */
 //	WriteDigitalOutputs(1); // Write to GPIO J15, pin 6; begin ISR timing pulse
 	int i;
-	data_flag = IsDataReady_UART2();
+	data_flag = IsDataReady_UART2(); //polls status of UART receive flag
+	if(data_flag != 0){
+	    data = Read_UART2();        // reads UART data (either 'a' or 'p')
+	}
 	float result = 0; //initialize the accumulator
+
 
  	if(CheckForOverrun())	// overrun error occurred (i.e. halted DSP)
 		return;				// so serial port is reset to recover
@@ -68,17 +73,21 @@ interrupt void Codec_ISR()
 	x_R_buffer[0] = CodecDataIn.Channel[LEFT];
 	x_E_buffer[0] = CodecDataIn.Channel[RIGHT];
 
-	//Filtering using a 32-bit accumulator
-	//Update filter history
+	// ANC mode: use adaptive filter to produce anti-noise
+	if (data == 'a' || data == 0){
+	    for (i = N-1; i>= 0; i--)       // calculate out of adaptive filt, updates weights and delays
+	    {
+	        result += (w[i] * x_R_buffer[i]);
+	        w[i] = alpha * w[i] + beta*x_R_buffer[0]*x_E_buffer[i];     //update weights with leaky algorithm
+	        x_R_buffer[i] = x_R_buffer[i-1];            //update reference delay samples
+	        x_E_buffer[i] = x_E_buffer[i-1];            //update error delay samples
+	    }
+	}
+	// passthrough mode: pass input sample directly to output
+	else if(data == 'p'){
+	    result = x_R_buffer[0];
 
-    for (i = N-1; i>= 0; i--)       // calculate out of adaptive filt, updates weights and delays
-    {
-        result += (w[i] * x_R_buffer[i]);
-        w[i] = alpha * w[i] + beta*x_R_buffer[0]*x_E_buffer[i];     //update weights with leaky algorithm
-        x_R_buffer[i] = x_R_buffer[i-1];            //update reference delay samples
-        x_E_buffer[i] = x_E_buffer[i-1];            //update error delay samples
-    }
-
+	}
 	//Return 16-bit sample to DAC
 	CodecDataOut.Channel[LEFT] = (short) result;
 

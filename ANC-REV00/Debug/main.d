dc63869bef3acf7c54cdab2a241ca43a435d5ed7
Eddie Gasca <17egc1@queensu.ca>
2019-11-25 20:54:01 -0500

    Added initialization of filter weights and delay buffers to StartUp.c. Also modified how weights are updated as per the LMS algorithm.

--- ANC-REV00/Debug/main.d
+++ ANC-REV00/Debug/main.d
@@ -5,15 +5,15 @@ main.obj: ../DSP_Config.h
 main.obj: ../OMAPL138_Support_DSP.h
 main.obj: ../tistdtypes.h
 main.obj: ../OMAPL138_defines.h
-main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h
-main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h
-main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h
+main.obj: C:/ti/ccs910/ccs/tools/compiler/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h
+main.obj: C:/ti/ccs910/ccs/tools/compiler/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h
+main.obj: C:/ti/ccs910/ccs/tools/compiler/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h
 
 ../main.c: 
 ../DSP_Config.h: 
 ../OMAPL138_Support_DSP.h: 
 ../tistdtypes.h: 
 ../OMAPL138_defines.h: 
-C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h: 
-C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h: 
-C:/Program\ Files\ (x86)/Texas\ Instruments/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/C6000\ Code\ Generation\ Tools\ 7.3.1/include/c6x.h: 
+C:/ti/ccs910/ccs/tools/compiler/C6000\ Code\ Generation\ Tools\ 7.3.1/include/vect.h: 
+C:/ti/ccs910/ccs/tools/compiler/C6000\ Code\ Generation\ Tools\ 7.3.1/include/stdint.h: 
--- ANC-REV00/Debug/subdir_rules.mk
+++ ANC-REV00/Debug/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C6000 Compiler'
-	"C:/Program Files (x86)/Texas Instruments/C6000 Code Generation Tools 7.3.1/bin/cl6x" -mv6740 -g --include_path="C:/Program Files (x86)/Texas Instruments/C6000 Code Generation Tools 7.3.1/include" --display_error_number --diag_warning=225 --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs910/ccs/tools/compiler/C6000 Code Generation Tools 7.3.1/bin/cl6x" -mv6740 -g --include_path="C:/ti/ccs910/ccs/tools/compiler/C6000 Code Generation Tools 7.3.1/include" --display_error_number --diag_warning=225 --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 %.obj: ../%.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C6000 Compiler'
-	"C:/Program Files (x86)/Texas Instruments/C6000 Code Generation Tools 7.3.1/bin/cl6x" -mv6740 -g --include_path="C:/Program Files (x86)/Texas Instruments/C6000 Code Generation Tools 7.3.1/include" --display_error_number --diag_warning=225 --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs910/ccs/tools/compiler/C6000 Code Generation Tools 7.3.1/bin/cl6x" -mv6740 -g --include_path="C:/ti/ccs910/ccs/tools/compiler/C6000 Code Generation Tools 7.3.1/include" --display_error_number --diag_warning=225 --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
